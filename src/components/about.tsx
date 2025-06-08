a57a195507b4a40c1aff62abe5ff34ae6adbdd81
RyannKim327 <rksesgundo123@gmail.com>
2025-06-08 20:14:23 +0800

    06-08-25 20:14 - @RyannKim327

---
+++ src/components/about.tsx
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <div>
+      <h3>
+        This application was based on King James Version and Magandang Balita
+        Bibliya versions, if ever that there's a problem with the answers, you
+        may send it{" "}
+        <a href="https://github.com/RyannKim327/bible-quiz/issues/1">HERE</a>
+      </h3>
+    </div>
+  );
+}
